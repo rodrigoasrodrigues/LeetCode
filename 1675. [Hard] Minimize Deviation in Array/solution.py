87bb197a52d5862249f3bf9d79d1e16b8e1816ad
Rodrigo Rodrigues <rodrigo@rodrigorodrigues.info>
2023-02-24 12:33:07 -0300

    accepted solution

--- 1675. [Hard] Minimize Deviation in Array/solution.py
+++ 1675. [Hard] Minimize Deviation in Array/solution.py
@@ -1,41 +1,48 @@
-# the description of the problem is really confusing
+import heapq
+
+
 class Solution:
-    def deviation(self, nums: List[int]) -> int:
-        'calculate deviation, assumes list sorted reverse'
-        minn = nums[-1]
+    def deviation(self, nums, minn) -> int:
+        'calculate deviation'
         maxn = nums[0]
-        dev = maxn - minn
+        dev = abs(maxn - minn)
         return dev
 
-    def double_all_odds(self, nums: List[int]) -> List[int]:
+    def double_all_odds(self, nums: list[int]) -> list[int]:
         'this applies the second operation to all'
         for i in range(len(nums)):
             if nums[i] % 2 == 1:
                 nums[i] = nums[i] * 2
         return nums
 
-    def halve_max(self, nums: List[int]) -> List[int]:
+    def halve_max(self, nums, minn):
         'assumes max to be even, otherwise returns None'
-        m = nums[0]
-        if m % 2 == 1: # if max is not event
+        m = heapq.heappop(nums)
+        if m % 2 != 0:  # if max is not even
             return None
-        nums[0] = nums[0] / 2
-        nums.sort(reverse=True)
-        return nums
-    
+        m = m / 2
+        if minn < m: # note that numbers are negative due to heapq
+            minn = m
+        heapq.heappush(nums, m)
+        return nums, minn
 
-
-    def minimumDeviation(self, nums: List[int]) -> int:
+    def minimumDeviation(self, nums) -> int:
         'iterate and minimizes deviation'
         # doubling all odds can only be done once and solves all possible cases for odds
         nums = self.double_all_odds(nums)
-        nums.sort(reverse=True)
-        current_lower_deviation = self.deviation(nums)
-        nums = self.halve_max(nums)
-        while nums: # while makes sense to attemp reduction
-            new_dev = self.deviation(nums)
-            if new_dev < current_lower_deviation: # if it reduces uses the new value
+        nums = [x * -1 for x in nums]
+        min_num = max(nums) # the list is negative for heap optimization so we use max
+        heapq.heapify(nums)
+        current_lower_deviation = self.deviation(nums, min_num)
+        nums, min_num = self.halve_max(nums, min_num)
+        while nums:  # while makes sense to attemp reduction
+            new_dev = self.deviation(nums, min_num)
+            if new_dev < current_lower_deviation:  # if it reduces uses the new value
                 current_lower_deviation = new_dev
-            nums = self.halve_max(nums)
+            halve = self.halve_max(nums, min_num)
+            if halve:
+                nums, min_num = halve
+            else:
+                break
         # return the lowest value obtained
         return int(current_lower_deviation)
