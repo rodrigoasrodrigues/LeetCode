aa79a64ea77dd41ebfdb8d5992693662fc796c78
Rodrigo Rodrigues <rodrigo@rodrigorodrigues.info>
2023-03-01 12:15:51 -0300

    merge sort solution

--- 912. [Medium] Sort an Array/solution.py
+++ 912. [Medium] Sort an Array/solution.py
@@ -1,21 +1,34 @@
 class Solution:
+    def merge(self, arr1, arr2):
+        'Merges sorted arrays' 
+        i = j = 0
+        l1 = len(arr1)
+        l2 = len(arr2)
+        res = []
+        arr1.append(0) # just to avoid index error
+        while (i < l1) or (j<l2):
+            if (j >= l2) :
+                res.append(arr1[i])
+                i += 1
+            elif arr1[i]<arr2[j] and (i < l1):
+                res.append(arr1[i])
+                i += 1
+            else:
+                res.append(arr2[j])
+                j += 1
+        return res
+    
+        
     def sortArray(self, nums: List[int]) -> List[int]:
-        #quick sort approach
+        #merge sort approach
         if len(nums) <= 1:
             return nums
         else:
-            pivo = nums[0]
-            maior = []
-            menor = []
-            iguais = [] # there may be more than one
-            for x in nums:
-                if x > pivo:
-                    maior.append(x)
-                elif x < pivo:
-                    menor.append(x)
-                else:
-                    iguais.append(x)
-            result = self.sortArray(menor)
-            result.extend(iguais)
-            result.extend(self.sortArray(maior))
-            return result
+            l = len(nums)
+            l2 = int(l/2)
+            left = nums[0:l2]
+            right = nums[l2:l]
+            left = self.sortArray(left)
+            right = self.sortArray(right)
+            res = self.merge(left,right)
+            return res
